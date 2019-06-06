b2855277f109e46823e46668ef17ef5f71ac9344
wizage <wizagesmax@gmail.com>
2019-06-05 22:03:57 -0700

    Initial Commit

---
+++ Workshop Test/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Workshop Test
+//
+//  Created by Sam Patzer on 6/5/19.
+//  Copyright © 2019 wizage. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
